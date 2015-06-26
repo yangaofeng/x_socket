f6ffa369d29d2123ab8ae53a7b4408b6febcfc8a
yangaofeng <dxxygf@163.com>
2015-06-26 20:36:23 +0800

    add linux/so_soname example code

---
+++ linux/so_soname/hello/hello.c
@@ -0,0 +1,8 @@
+// hello.cc (2015-06-26)
+// Yan Gaofeng (yangaofeng@360.cn)
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("hello, library world\n");
+}
---
+++ linux/so_soname/hello/hello.h
@@ -0,0 +1,4 @@
+// hello.h (2015-06-26)
+// Yan Gaofeng (yangaofeng@360.cn)
+
+void hello(void);
