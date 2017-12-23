6f610171af4184f9e2684ddc28fbe6a5c211eb95
yangaofeng <yangaofeng@b.360.cn>
2017-12-23 13:42:46 +0800

    update mfc/rtti, add dynamic create example

--- mfc/rtti/main.cc
+++ mfc/rtti/main.cc
@@ -2,6 +2,8 @@
 // Yan Gaofeng (yan_gaofeng@hotmail.com)
 
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 #define  LPCSTR char *
 #define  UINT unsigned int
@@ -12,13 +14,26 @@ struct CRuntimeClass
 {
     // Attributes
     LPCSTR m_lpszClassName;
+
     int m_nObjectSize;
-    UINT m_wSchema; // schema number of the loaded class
+
+    // schema number of the loaded class
+    UINT m_wSchema; 
+
+    // 由IMPLEMENT_DYNCREATE宏指定
     CObject* (* m_pfnCreateObject)(); // NULL => abstract class
+
     CRuntimeClass* m_pBaseClass;
+
+    // 会使用 m_pfnCreateObject 创建具体的对象
+    CObject* CreateObject();
+
+    // CRuntimeClass 有, 其它类没有
+    static CRuntimeClass* Load();
 
     // CRuntimeClass objects linked together in simple list
     static CRuntimeClass* pFirstClass; // start of class list
+
     CRuntimeClass* m_pNextClass; // linked list of registered classes
 };
 
@@ -56,6 +71,17 @@ AFX_CLASSINIT::AFX_CLASSINIT(CRuntimeClass *pNewClass)
     _IMPLEMENT_RUNTIMECLASS(class_name, base_class_name, 0xFFFF, NULL)
 
 
+// 动态创建
+#define DECLARE_DYNCREATE(class_name) \
+    DECLARE_DYNAMIC(class_name) \
+    static CObject* CreateObject();
+#define IMPLEMENT_DYNCREATE(class_name, base_class_name) \
+    CObject* class_name::CreateObject() \
+    { return new class_name; } \
+    _IMPLEMENT_RUNTIMECLASS(class_name, base_class_name, 0xFFFF, \
+        class_name::CreateObject)
+
+
 
 // CObject, 表头, 需要特殊构造, 不能直接套用宏
 class CObject {
@@ -64,6 +90,9 @@ class CObject {
         virtual CRuntimeClass *GetRuntimeClass() const;
 
         bool IsKindOf(const CRuntimeClass *pClass) const;
+
+        virtual void SayHello() { std::cout << "Hello Object\n"; }
+
     public:
         static CRuntimeClass classCObject;
 };
@@ -99,6 +128,38 @@ CRuntimeClass *CObject::GetRuntimeClass() const
 // CObject 表头构造结束
 
 
+CObject* CRuntimeClass::CreateObject()
+{
+    if (m_pfnCreateObject == NULL) {
+        printf("Error: Trying to create object which is not "
+                "DECLARE_DYNCREATE \nor DECLARE_SERIAL: %hs.\n",
+                m_lpszClassName);
+        return NULL;
+    }
+
+    CObject* pObject = NULL;
+    // 调用具体类的创建函数
+    pObject = (*m_pfnCreateObject)();
+    return pObject;
+}
+
+// 模拟用的, 从命令行创建类
+CRuntimeClass* CRuntimeClass::Load()
+{
+    char szClassName[64];
+    CRuntimeClass* pClass;
+    // JJHOU : instead of Load from file, we Load from cin.
+    std::cout << "enter a class name... ";
+    std::cin >> szClassName;
+    for (pClass = pFirstClass; pClass != NULL; pClass = pClass->m_pNextClass) {
+        if (strcmp(szClassName, pClass->m_lpszClassName) == 0)
+            return pClass;
+    }
+    printf("Error: Class not found: %s \n", szClassName);
+    return NULL; // not found
+}
+
+
 // CCmdTarget
 class CCmdTarget : public CObject {
     DECLARE_DYNAMIC(CCmdTarget)
@@ -109,8 +170,17 @@ class CWinThread: public CCmdTarget {
     DECLARE_DYNAMIC(CWinThread)
 };
 
+// CWind
+class CWnd: public CCmdTarget {
+    DECLARE_DYNCREATE(CWnd)
+
+public:
+    void SayHello() { std::cout << "Hello CWnd \n"; }
+};
+
 IMPLEMENT_DYNAMIC(CCmdTarget, CObject)
 IMPLEMENT_DYNAMIC(CWinThread, CCmdTarget)
+IMPLEMENT_DYNCREATE(CWnd, CCmdTarget)
 
 
 void PrintAllClasses()
@@ -129,10 +199,25 @@ void PrintAllClasses()
 int main() {
     PrintAllClasses();
 
+    // 演示运行时类型识别
     CWinThread o;
     bool ok = o.IsKindOf(RUNTIME_CLASS(CCmdTarget));
     if (ok) {
         std::cout << "CWinThread is s CCmdTarget" << "\n";
+    }
+
+    // 演示动态创建, 请在命令行输入"CWnd"
+    CRuntimeClass *pClassRef;
+    CObject *pOb;
+    while(1) {
+        if ((pClassRef = CRuntimeClass::Load()) == NULL) {
+            break;
+        }
+
+        pOb = pClassRef->CreateObject();
+        if (pOb != NULL) {
+            pOb->SayHello();
+        }
     }
 
     return 0;
